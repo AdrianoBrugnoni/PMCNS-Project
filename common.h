abc8254ea9d246a32d9126add06a578795e4953b
alessandrofato98 <alessandro.f98@hotmail.com>
2021-12-24 10:42:32 +0100

    refactoring :O

--- common.h
+++ common.h
@@ -1,6 +1,6 @@
 #define START 0					// inizio all'ora 0
 #define END 24*90				// termine simulazione al giorno 90
-#define INF 100*END			// tempo irraggiungibile
+#define INF 100*END			    // tempo irraggiungibile
 
 #define COVID 0					// Tipo 1
 #define NCOVID 1				// Tipo 2
